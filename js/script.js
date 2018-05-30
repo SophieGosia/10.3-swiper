eb10005ded0907048fdeaa631b46cd740ab8acec
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-05-30 19:13:13 +0200

    Update README.MD

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
   const $slides = $swiper.find('.swiper__slides');
   const $slideList = $swiper.find('.swiper__slide');
 
-  // Pobieram wielkość swipera
+  // get swiper width
   const swiperWidth = $swiper.outerWidth();
-  // Zmienna obecnej pozycji i ilośc slajdów
+  // current position let and const with number of slides
   const slidesCounter = $slideList.length;
   let currentSwipe = 0;
 
-  // Automatyczna zmiana slajdów
+  // auto slide change
 
   function autoMove() {
     console.log('Move auto');
@@ -23,12 +23,12 @@ $(document).ready(function () {
   }
   setInterval(autoMove, 3000);
 
-  // Ustawiam wszystkim pojedynczym wielkość jak swipera
+  // set all slides width the same as swiper's
   $slideList.each(function () {
     $(this).width(swiperWidth);
   });
 
-  // Tworze kulki
+  // make meatballs
   for (let i = 0; i < slidesCounter; i++) {
     $('<div />', {
       class: 'swiper__bullet'
@@ -38,29 +38,32 @@ $(document).ready(function () {
     }).appendTo('#swiper-bullets');
   }
 
-  // Zapinam akcje przesuwania
+  // add swipe action to arrows
   $('#swiper-prev').on('click', movePrev);
   $('#swiper-next').on('click', moveNext);
 
+  // add swiping to move back arrow
   function movePrev() {
     if (currentSwipe === 0) {
       console.log('First slide we cannot go below 0');
       return;
     }
-    // Zmniejsz obecny slajd o jeden
+    // go to previous slide
     currentSwipe--;
     translate(currentSwipe);
   }
-
+  // add swiping to move forward arrow
   function moveNext() {
     if (currentSwipe === slidesCounter - 1) {
       console.log('Last slide');
       return;
     }
+    // go to next slide
     currentSwipe++;
     translate(currentSwipe);
   }
 
+  // repositioning on x axis to proper slide
   function translate(position) {
     var offset = position * swiperWidth;
     console.log('Translate to slide', position);
